ac07abfcf6a5c63413c6e787382e9334bf91afbb
ArthurAsam <asa.arthur@icloud.com>
2024-09-30 10:52:06 -0500

    Add logic to play 5 rounds
    Added if/else statement for declaring the winner
    of the 5 rounds with final score printed.
    In the for loop I have the fn call for human and
    computer choice that way I get a new random choice
    on each iteration.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let playerScore = 0; 
 let computerScore = 0 ; 
 
+
 function getComputerChoice(){
   let randomNumber = Math.floor(Math.random()* 3)
   let computerChoice; 
@@ -13,42 +14,62 @@ function getComputerChoice(){
   }
 }
 
-let getHumanChoice = function(){
+function getHumanChoice(){
   return prompt('Pick a choice between "rock" "paper" or "scissors"', '')
 }; 
 
-const computerSelection = getComputerChoice(); 
-const humanSelection = getHumanChoice().toLowerCase()
 
-function playRound(humanSelection, computerSelection){
- if(humanSelection === 'paper' && computerSelection === 'rock'){
-  ++playerScore
-  console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
-  return "You Won! Paper beats Rock"
+function playGame(humanSelection, computerSelection){
+  playRound(humanSelection, computerSelection)
 
- } else if(humanSelection === 'paper' && computerSelection === 'scissors'){
-  ++computerScore
-  console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
-  return "You Lose! Scissors beats Paper"
- } else if (humanSelection === 'rock' && computerSelection === 'paper'){
-  ++computerScore
-  console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
-  return 'You Lose! Paper beats Rock'
- } else if(humanSelection === 'rock' && computerSelection === 'scissors'){
-  ++playerScore
-  console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
-  return 'You Won! Rock beats Scissors'
- } else if(humanSelection === 'scissors' && computerSelection === 'paper'){
-  ++playerScore
-  console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
-  return 'You Won! Scissors beats Paper'
- } else if(humanSelection === 'scissors' && computerSelection === 'rock'){
-  ++computerScore
-  console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
-  return 'You lose! Rock beats Scissors'
- } else { 
-  return 'It\'s a draw!'
- }
+  function playRound(humanSelection, computerSelection){
+    if(humanSelection === 'paper' && computerSelection === 'rock'){
+     ++playerScore
+     console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
+     console.log("You Won! Paper beats Rock")
+   
+    } else if(humanSelection === 'paper' && computerSelection === 'scissors'){
+     ++computerScore
+     console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
+     console.log("You Lose! Scissors beats Paper")
+    } else if (humanSelection === 'rock' && computerSelection === 'paper'){
+     ++computerScore
+     console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
+     console.log('You Lose! Paper beats Rock')
+    } else if(humanSelection === 'rock' && computerSelection === 'scissors'){
+     ++playerScore
+     console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
+     console.log('You Won! Rock beats Scissors')
+    } else if(humanSelection === 'scissors' && computerSelection === 'paper'){
+     ++playerScore
+     console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
+     console.log('You Won! Scissors beats Paper')
+    } else if(humanSelection === 'scissors' && computerSelection === 'rock'){
+     ++computerScore
+     console.log(`Player1: ${playerScore} Computer: ${computerScore}`)
+     console.log('You lose! Rock beats Scissors')
+    } else { 
+     console.log('That\'s a draw!')
+    }
+   }
+
+  
 }
 
-console.log(playRound(humanSelection,computerSelection))
+for (let i = 0; i < 5; i++){
+  let humanSelection = getHumanChoice()
+  let computerSelection = getComputerChoice()
+  playGame(humanSelection, computerSelection)
+  
+ }
+
+ console.log(`Final score; Player1: ${playerScore} | Computer: ${computerScore}`)
+ if (playerScore > computerScore){
+  console.log('Player1 Won the Game!')
+ } else if (computerScore > playerScore){
+  console.log('Computer Won the Game!')
+ } else  {
+  console.log('It\'s a tie match!')
+ }
+ playerScore = 0 ; 
+ computerScore = 0
